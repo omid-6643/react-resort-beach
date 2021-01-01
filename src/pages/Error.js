52f2f1c5550d279bd25926ae0cf3c8ef55e43f4d
omid beigi <o.beygi@gamil.com>
2021-01-01 14:06:52 +0330

    second

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-    return (
-        <div>
-            hello from error 
-        </div>
-    )
-}
+  return (
+    <Hero>
+      <Banner title="404" subtitle="page not found">
+        <Link to="/" className="btn-primary">
+          return Home
+        </Link>
+      </Banner>
+    </Hero>
+  );
+};
 
-export default Error
+export default Error;
