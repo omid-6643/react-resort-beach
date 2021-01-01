c92bbe060650776636c0584532018ed20063ef73
omid beigi <o.beygi@gamil.com>
2021-01-01 12:22:01 +0330

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import "./App.css";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
 
 function App() {
   return (
-    <div className="App">
-     hello
-    </div>
+    <>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms/" component={Rooms} />
+      <Route exact path="/rooms/:slug" component={SingleRoom} />
+    </>
   );
 }
 
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SingleRoom extends Component {
+    render() {
+        return (
+            <div>
+                hello from single room
+            </div>
+        );
+    }
+}
+
+export default SingleRoom;
